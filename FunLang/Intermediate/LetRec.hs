7ab6af882b3d8a143fff68535cca81b6dcb791be
Riku Salminen (riku dot salminen (at) tkk dot fi) <Riku Salminen (riku dot salminen (at) tkk dot fi)@localhost>
2010-04-03 19:23:27 +0300

    More LetRec fun

--- FunLang/Intermediate/LetRec.hs
+++ FunLang/Intermediate/LetRec.hs
@@ -2,10 +2,10 @@ module FunLang.Intermediate.LetRec where
 
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import qualified Data.List as List
 
 import FunLang.Intermediate.Desugared
 
-desugarLetRec :: [(Identifier, Desugared)] -> Map.Map Identifier (Set.Set Identifier)
 
 closure :: Ord a => Map.Map a (Set.Set a) -> Set.Set a -> Set.Set a
 closure recurse set =
@@ -18,15 +18,28 @@ closure recurse set =
         unbound = [expand | (x, Just expand) <- zip list (map (\x -> Map.lookup x recurse) list), not (Set.member x bound)]
         expand = Set.unions (map (closure' (Set.union bound set)) unbound)        
 
-desugarLetRec decls =
-    recurse
+recursiveDecls :: [(Identifier, Desugared)] -> Map.Map Identifier (Set.Set Identifier)
+recursiveDecls decls =
+    Map.fromList recurse
     where
     locals = map fst decls
     localset = Set.fromList locals
     freeVars = map (freeVariables . snd) decls
-    recurse = Map.fromList
-        [(identifier, free) | (identifier, free) <- zip locals freeVars, not (Set.null (Set.intersection localset free))]
-    depends = closure recurse
+    recurse = [(identifier, free) | (identifier, free) <- zip locals freeVars, not (Set.null (Set.intersection localset free))]
 
---        Set.unions [closure x vars | (x, Just vars) <- map (\x -> x, map (Map.lookup x recurse)) (Set.toList set), x /= identifier]
+depends :: Map.Map Identifier (Set.Set Identifier) -> Map.Map Identifier (Set.Set Identifier)
+depends recurse = Map.map (closure recurse) recurse
+
+scopes :: Map.Map Identifier (Set.Set Identifier) -> [([Identifier], [Identifier])]
+scopes depmap =
+    (List.sortBy smaller) . map listify . Map.toList $ scopemap
+    where
+    listify (set,x) = (Set.toList set, x)
+    smaller (list1,_) (list2,_) = compare (length list1) (length list2)
+    scopemap = foldl add Map.empty (Map.toList depmap)
+    add scopemap (identifier,deps) =
+        Map.alter alter deps scopemap
+        where
+        alter Nothing = Just [identifier]
+        alter (Just list) = Just (identifier : list)
 
--- test/mutual.fun
+++ test/mutual.fun
@@ -1,3 +1,4 @@
+{-
 let
     -- even n = if n then (odd (n+neg 1)) else 1
     -- odd n = if n then (even (n+neg 1)) else 0
@@ -14,3 +15,29 @@ let
     in
     isEven 12 + isOdd 12
 
+let ev = (lambda ev od n: if n then (od ev od (n+neg 1)) else 1),
+    od = (lambda ev od n: if n then (ev ev od (n+neg 1)) else 0)
+in
+    let even = (lambda even odd: even even odd) ev od,
+        odd = (lambda even odd: odd even odd) ev od
+    in
+        even 12 + odd 12
+    
+(lambda ev od:
+    let even = (lambda even odd: even even odd) ev od,
+        odd = (lambda even odd: odd even odd) ev od
+    in even 12 + odd 12)
+    (lambda ev od n: if n then (od ev od (n+neg 1)) else 1),
+    (lambda ev od n: if n then (ev ev od (n+neg 1)) else 0)
+
+-}
+
+(lambda ev od:
+    (lambda even odd: even 12 + odd 12)
+        ((lambda even odd: even even odd) ev od)
+        ((lambda even odd: odd even odd) ev od)
+    )
+    (lambda ev od n: if n then (od ev od (n+neg 1)) else 1)
+    (lambda ev od n: if n then (ev ev od (n+neg 1)) else 0)
+    
+        
